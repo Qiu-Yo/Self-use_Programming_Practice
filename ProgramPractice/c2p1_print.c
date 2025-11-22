87ac4e23839b9fb605bb79977a395b5c8a491561
Qiu-Yo <159090538+Qiu-Yo@users.noreply.github.com>
2025-11-22 10:47:57 +0800

    Resolve merge conflict in c2p1_print.c
    Removed conflicting code and retained the final version of the program.

--- ProgramPractice/c2p1_print.c
+++ ProgramPractice/c2p1_print.c
@@ -1,4 +1,3 @@
-<<<<<<<< HEAD:ProgramPractice/c2p1_print.c
 // 编写一个程序把你的名字以奇怪的形式输出
 // 这个程序用以熟悉换行标识符的\n用法
 
@@ -11,17 +10,3 @@ int main(void)
     printf("Yo");
     return 0;
 }
-========
-// 编写一个程序把你的名字以奇怪的形式输出
-// 这个程序用以熟悉换行标识符的\n用法
-
-#include <stdio.h>
-int main(void)
-{
-    printf("Qiu Yo\n");
-    printf("Qiu\nYo\n");
-    printf("Qiu ");
-    printf("Yo");
-    return 0;
-}
->>>>>>>> b17be248095f12230dac455d413920f332763275:ProgramPractice/P1-打印.c
