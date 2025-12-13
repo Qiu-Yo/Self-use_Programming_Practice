f5e9bb5865fa4e5c51d6ad883b972c5738257ebf
Qiu-Yo <nsjis1@163.com>
2025-12-13 21:34:30 +0800

    添加了1项新的编程练习项目；

---
+++ TextbookProgramPractice/c10p2.c
@@ -0,0 +1,56 @@
+#include <stdio.h>
+void copy_arr(double target_array[], const double source_array[], size_t array_length)
+{
+    for (int i = 0; i < array_length; i++)
+    {
+        target_array[i] = source_array[i];
+    }
+}
+void copy_ptr(double *target_array, const double *source_array, size_t array_length)
+{
+    for (int i = 0; i < array_length; i++)
+    {
+        *target_array = *source_array;
+        target_array++;
+        source_array++;
+    }
+}
+void copy_ptrs(double target_array[], const double source_array[], double *next_element)
+{
+    for (; source_array < next_element; source_array++)
+    {
+        *target_array = *source_array;
+        target_array++;
+    }
+}
+int main()
+{
+    double source[5] = {1.2, 2.3, 3.4, 4.5, 5.6};
+    double target1[5] = {0};
+    double target2[5] = {0};
+    double target3[5] = {0};
+    copy_arr(target1, source, 5);
+    copy_ptr(target2, source, 5);
+    copy_ptrs(target3, source, source + 5);
+
+    for (int i = 0; i < 5; i++)
+    {
+        printf("%lf ", source[i]);
+    }
+    printf("\n");
+    for (int i = 0; i < 5; i++)
+    {
+        printf("%lf ", target1[i]);
+    }
+    printf("\n");
+    for (int i = 0; i < 5; i++)
+    {
+        printf("%lf ", target2[i]);
+    }
+    printf("\n");
+    for (int i = 0; i < 5; i++)
+    {
+        printf("%lf ", target3[i]);
+    }
+    printf("\n");
+}
